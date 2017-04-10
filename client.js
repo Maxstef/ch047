d92f15e1fc63e765920edeee726d0f8d92483ce7
VARANKIN_OLEKSANDR <alexandr1337@gmail.com>
2017-04-10 16:38:37 +0300

    1

--- client.js
+++ client.js
@@ -7,9 +7,7 @@ var express = require('express'),
 app.use(express.static('dist'));
 app.set('views', path.join(__dirname, 'dist'));
 // app.set('view engine', 'js');
-// app.set("view options", {layout: false});
-app.use(app.router);
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
     res.render(path.join('dist/index.html'));
 });
 
@@ -17,7 +15,3 @@ app.set('port', (process.env.PORT || 5000));
 
 console.log('Server up and running on http://localhost:/' + app.get('port'));
 app.listen(app.get('port'));
-//
-
-// app.set('views', path.join(__dirname, 'app'));
-// app.set('view engine', 'html');
