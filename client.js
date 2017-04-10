99105fc545a10b31c4b7e8202bf20507792eb098
VARANKIN_OLEKSANDR <alexandr1337@gmail.com>
2017-04-10 16:20:55 +0300

    1

--- client.js
+++ client.js
@@ -8,14 +8,8 @@ app.use(express.static('dist'));
 app.set('views', path.join(__dirname, 'dist'));
 // app.set('view engine', 'js');
 
-app.use(function(req, res, next) {
-    if (req.path === '/*')
-        return res.redirect('/');
-    next();
-});
-
 app.get('*', function (req, res) {
-    res.sendFile(path.join('dist/index.html'));
+    // res.sendFile(path.join('dist/index.html'));
 });
 
 app.set('port', (process.env.PORT || 5000));
