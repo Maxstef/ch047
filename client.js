ad414ca73409ee26e44484093e4aa1fd2b895ea1
VARANKIN_OLEKSANDR <alexandr1337@gmail.com>
2017-04-10 16:32:17 +0300

    1

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ app.use(express.static('dist'));
 app.set('views', path.join(__dirname, 'dist'));
 // app.set('view engine', 'js');
 app.set("view options", {layout: false});
-app.use(app.router);
+// app.use(app.router);
 app.get('*', function (req, res) {
     res.render(path.join('dist/index.html'));
 });
